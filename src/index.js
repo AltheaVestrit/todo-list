0bedbbbd7f4de8c09cde0c1ed99d2cf71b86f9fc
work <137403706+AltheaVestrit@users.noreply.github.com>
2025-07-23 15:53:02 +0200

    Test localStorage functionality

--- src/index.js
+++ src/index.js
@@ -16,3 +16,33 @@ import myModule from './myModule.js';
 // import css:
 import './styles.css';
 */
+
+console.log("[Local Storage Test] Starting test...")
+
+localStorage.setItem("myKey", "myValue");
+console.log(localStorage.getItem("myKey"));
+
+const myTask1 = {
+    name: "Do the laundry",
+    dueDate: "23-07-2023",
+    priority: "medium",
+    description: "Wash the colored clothes, and maybe the dark clothes as well",
+    status: "to do"
+};
+
+const myTask2 = {
+    name: "Wash dishes",
+    dueDate: "23-07-2023",
+    priority: "high",
+    description: "Handwash wooden utensils and run dishwasher",
+    status: "done"
+}
+
+const tasksArray = [myTask1, myTask2];
+
+
+
+localStorage.setItem("tasks", JSON.stringify(tasksArray));
+console.log(JSON.parse(localStorage.getItem("tasks")));
+
+console.log("[Local Storage Test] Test completed!")
