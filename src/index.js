b734c169a5b6364af1eb2b81d0b00e574ce5c9b8
work <137403706+AltheaVestrit@users.noreply.github.com>
2025-07-28 13:31:57 +0200

    Fix tests in index.js file

--- src/index.js
+++ src/index.js
@@ -22,14 +22,22 @@ import myProjects from "./Projects.js";
 // ====================== TESTING ======================== //
 
 // PROJECT MODULES
-// CREATE
 console.log("=================================");
 console.log("INITIALIZE PROJECTS IIFE");
 console.log(myProjects.returnProjects());
+const firstProject = myProjects.returnProjectIDs()[0];
 
+// CREATE
 console.log("=================================");
 console.log("ADD NEW PROJECT");
 myProjects.createNewProject("My new project");
+console.log(myProjects.returnProjects());
+const newProject = myProjects.returnProjectIDs()[1];
+
+// DELETE
+console.log("=================================");
+console.log("DELETE PROJECT");
+console.log("Deletion succesful: " + myProjects.deleteProject(firstProject));
 console.log(myProjects.returnProjects());
 
 // RETURN
@@ -44,18 +52,12 @@ console.log(firstProject, myProjects.returnProject(firstProject));
 // UPDATE
 console.log("=================================");
 console.log("UPDATE PROJECT NAME");
-const firstProject = myProjects.returnProjectIDs()[0];
 console.log(
-  "Update succesful: " + myProjects.updateProject(firstProject, "Blabla")
+  "Update succesful: " + myProjects.updateProject(newProject, "Blabla")
 );
 console.log(myProjects.returnProjects());
 
 // DELETE
-console.log("=================================");
-console.log("DELETE PROJECT");
-console.log("Deletion succesful: " + myProjects.deleteProject(firstProject));
-console.log(myProjects.returnProjects());
-
 console.log("=================================");
 console.log("TRY TO DELETE NON-EXISTING PROJECT");
 console.log("Deletion succesful: " + myProjects.deleteProject(firstProject)); //firstProject was already deleted before
@@ -65,7 +67,6 @@ console.log(myProjects.returnProjects());
 // CREATE
 console.log("=================================");
 console.log("ADD NEW TASK TO EXISTING PROJECT");
-const newProject = myProjects.returnProjectIDs()[0];
 console.log(
   "Task created: " +
     myProjects.createNewTask(newProject, {
@@ -74,6 +75,7 @@ console.log(
     })
 );
 console.log(myProjects.returnTasks(newProject));
+const myTask = myProjects.returnTaskIDs(newProject)[0];
 
 console.log("=================================");
 console.log("TRY TO ADD NEW TASK TO NON-EXISTING PROJECT");
@@ -98,7 +100,6 @@ console.log(myProjects.returnTasks(newProject));
 // UPDATE
 console.log("=================================");
 console.log("UPDATE EXISTING TASK");
-const myTask = myProjects.returnTaskIDs(newProject)[0];
 console.log(
   "Task updated: " +
     myProjects.updateTask(newProject, myTask, {
